278498cf61ece38ad87af08231b5040046c70714
何烨霖 <chaodoing@hotmail.com>
2023-09-29 01:20:47 +0800

    添加密码加密函数

--- encrypt/encrypt.go
+++ encrypt/encrypt.go
@@ -2,10 +2,13 @@ package encrypt
 
 import (
 	`crypto/md5`
+	`crypto/sha1`
+	`crypto/sha256`
 	`encoding/base64`
 	`fmt`
-	`github.com/google/uuid`
 	`net/url`
+	
+	`github.com/google/uuid`
 )
 
 // Md5 加密字符串
@@ -18,10 +21,26 @@ func Md5(value string) string {
 }
 
 // Password 加密密码
-//  @param password string 要加密的密码
-//  @return passwords string 加密后的密码
+//  password 要加密的密码
+//  passwords 加密后的密码
 func Password(password string) (passwords string) {
 	passwords = Md5(url.QueryEscape(base64.StdEncoding.EncodeToString([]byte(password))))
+	return
+}
+
+// PasswdSHA1 SHA1密码加密
+func PasswdSHA1(s string) (p string) {
+	o := sha1.New()
+	o.Write([]byte(s))
+	p = base64.StdEncoding.EncodeToString(o.Sum(nil))
+	return
+}
+
+// PasswdSHA256 SHA256密码加密
+func PasswdSHA256(s string) (p string) {
+	o := sha256.New()
+	o.Write([]byte(s))
+	p = base64.StdEncoding.EncodeToString(o.Sum(nil))
 	return
 }
 
